970d96f2e5998574c9d83b8815a5d2f7d9b2ed7d
Sarah Franc <sfranc@MacBook-Air-de-Sarah.local>
2018-06-21 11:18:56 +0200

    move from free to in use generic function 2

--- srcs/block.c
+++ srcs/block.c
@@ -14,7 +14,23 @@ void	ft_delete_block(t_block **start, t_block *block)
 
 	if (block->next)
 		block->next->prev = block->prev;
+}
 
+void	ft_add_block_top(t_block **start, t_block *block)
+{
+	if (!*start)
+	{
+		*start = block;
+		block->next = NULL;
+		block->prev = NULL;
+	}
+	else
+	{
+		(*start)->prev = block;
+		block->next = *start;
+		block->prev = NULL;
+		*start = block;
+	}
 }
 
 void	ft_move_block_to_use(int type, t_block *block)
@@ -39,22 +55,23 @@ void	ft_move_block_to_use(int type, t_block *block)
 	ft_putendl("--- free");
 
 	// add to in_use
-	if (!g_heap[type].in_use)
-	{
-		ft_putendl("in_use is empty");
-		g_heap[type].in_use = block;
-		block->next = NULL;
-		block->prev = NULL;
-	}
-	else
-	{
-		ft_putendl("in_use is not empty");
+	ft_add_block_top(&g_heap[type].free, block);
+	// if (!g_heap[type].in_use)
+	// {
+	// 	ft_putendl("in_use is empty");
+	// 	g_heap[type].in_use = block;
+	// 	block->next = NULL;
+	// 	block->prev = NULL;
+	// }
+	// else
+	// {
+	// 	ft_putendl("in_use is not empty");
 
-		g_heap[type].in_use->prev = block;
-		block->next = g_heap[type].in_use;
-		block->prev = NULL;
-		g_heap[type].in_use = block;
-	}
+	// 	g_heap[type].in_use->prev = block;
+	// 	block->next = g_heap[type].in_use;
+	// 	block->prev = NULL;
+	// 	g_heap[type].in_use = block;
+	// }
 
 	ft_putendl("--- in use");
 	ft_show_heap(g_heap[type].in_use);
