2c2f9fe3835fc407056d8730680bfe45ead023b0
Sarah Franc <sfranc@MacBook-Air-de-Sarah.local>
2018-07-02 22:20:20 +0200

    print memory norme

--- srcs/ft_print_memory.c
+++ srcs/ft_print_memory.c
@@ -1,20 +1,50 @@
 #include "malloc.h"
 
-void	ft_print_memory(const void *addr, size_t size)
+void	ft_dump_hexa(const void *addr)
 {
 	char	hex[16] = "0123456789abcdef";
-	char	s[16] = "0000000000000000";
 	int		temp;
+
+	temp = *(char*)addr >> 4 & 0x0F;
+	write(1, &hex[temp], 1);
+	temp = *(char*)addr & 0x0F;
+	write(1, &hex[temp], 1);
+}
+
+void	ft_print_last_line(int i, char s[16])
+{
+	size_t	a;
+
+	if (i % 16 != 0)
+	{
+		a = i;
+		while (a % 16 != 0)
+		{
+			write(1, "  ", 2);
+			if (a % 2 == 0)
+				write(1, " ", 1);
+			a++;
+		}
+		if (i % 2 == 1)
+			write(1, " ", 1);
+		write(1, s, i % 16);
+		write(1, "\n", 1);
+	}
+
+	write(1, "\n", 1);	
+
+}
+
+void	ft_print_memory(const void *addr, size_t size)
+{
+	char	s[16] = "0000000000000000";
 	size_t	i;
 	size_t	a;
 
 	i = 0;
 	while (i < size)
 	{
-		temp = *(char*)addr >> 4 & 0x0F;
-		write(1, &hex[temp], 1);
-		temp = *(char*)addr & 0x0F;
-		write(1, &hex[temp], 1);
+		ft_dump_hexa(addr);
 		s[i % 16] = *(char*)addr;
 		if (s[i % 16] < 32 || s[i % 16] > 126)
 			s[i % 16] = '.';
@@ -31,20 +61,5 @@ void	ft_print_memory(const void *addr, size_t size)
 		}
 		addr++;
 	}
-	if (i % 16 != 0)
-	{
-		a = i;
-		while (a % 16 != 0)
-		{
-			write(1, "  ", 2);
-			if (a % 2 == 0)
-				write(1, " ", 1);
-			a++;
-		}
-		if (i % 2 == 1)
-			write(1, " ", 1);
-		write(1, s, i % 16);
-		write(1, "\n", 1);
-	}
-	write(1, "\n", 1);
+	ft_print_last_line(i, s);
 }
