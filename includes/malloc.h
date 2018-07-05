d160ac20e2e9d99641809fc05285177e69fe5630
Sarah FRANC <sfranc@e2r1p10.42.fr>
2018-07-05 19:19:51 +0200

    norme include

--- includes/malloc.h
+++ includes/malloc.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   malloc.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sfranc <sfranc@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/05 18:01:32 by sfranc            #+#    #+#             */
+/*   Updated: 2018/07/05 19:19:38 by sfranc           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MALLOC_H
 # define MALLOC_H
 
@@ -23,14 +35,14 @@
 # define STAT_ENV		"MALLOC_STAT"
 # define LOG_ENV		"MALLOC_LOG"
 
-typedef struct 		s_block
+typedef struct			s_block
 {
 	size_t				size;
 	struct s_block		*prev;
 	struct s_block		*next;
 }						t_block;
 
-typedef struct s_heap
+typedef struct			s_heap
 {
 	t_block				*in_use;
 	t_block				*free;
@@ -38,16 +50,16 @@ typedef struct s_heap
 }						t_heap;
 
 t_heap					g_heap[2];
-extern pthread_mutex_t 	g_mutex;
+extern pthread_mutex_t	g_mutex;
 
-enum e_heap
+enum					e_heap
 {
 	TINY,
 	SMALL,
 	LARGE
 };
 
-enum e_lib
+enum					e_lib
 {
 	MALLOC,
 	FREE,
@@ -55,78 +67,81 @@ enum e_lib
 	CALLOC
 };
 
-void		*malloc(size_t size);
-void		*calloc(size_t count, size_t size);
-void		free(void *ptr);
-void		*realloc(void *ptr, size_t size);
-void		show_alloc_mem(void);
-void		show_alloc_mem_hex(void);
+void					*malloc(size_t size);
+void					*calloc(size_t count, size_t size);
+void					free(void *ptr);
+void					*realloc(void *ptr, size_t size);
+void					show_alloc_mem(void);
+void					show_alloc_mem_hex(void);
 
 /*
 ** FREE POOL
 */
-t_block		*ft_choose_free_block(int type, size_t size);
-t_block 	*ft_add_free_block(t_block *last, int type, size_t size);
-t_block		*ft_extend_free_pool(void *last, int type, size_t size);
-t_block		*ft_request_memory(void *last, size_t size);
-void		ft_move_block_to_use(int type, t_block *block);
+t_block					*ft_choose_free_block(int type, size_t size);
+t_block					*ft_add_free_block(t_block *last, int type,\
+							size_t size);
+t_block					*ft_extend_free_pool(void *last, int type, size_t size);
+t_block					*ft_request_memory(void *last, size_t size);
+void					ft_move_block_to_use(int type, t_block *block);
 
 /*
 ** IN USE POOL
 */
-int 		ft_find_used_block(void *ptr, t_block **block);
-void		ft_move_block_to_free(int type, t_block *block);
+int						ft_find_used_block(void *ptr, t_block **block);
+void					ft_move_block_to_free(int type, t_block *block);
 
 /*
 ** FREE
 */
-void		ft_free_block(int type, t_block *block);
-void		ft_defragmentation(int type);
+void					ft_free_block(int type, t_block *block);
+void					ft_defragmentation(int type);
 
 /*
 ** BLOCKS
 */
-t_block		*ft_split_block(t_block *block, int type, size_t size);
-void		ft_delete_block(t_block **start, t_block *block);
-void		ft_insert_block_top(t_block **start, t_block *block);
-void		ft_insert_block_top_defrag(t_block **start, t_block *block);
-int			ft_insert_block_addr(t_block **start, t_block *new_block);
+t_block					*ft_split_block(t_block *block, int type, size_t size);
+void					ft_delete_block(t_block **start, t_block *block);
+void					ft_insert_block_top(t_block **start, t_block *block);
+void					ft_insert_block_top_defrag(t_block **start,\
+							t_block *block);
+int						ft_insert_block_addr(t_block **start,\
+							t_block *new_block);
 
 /*
 ** TOOLS
 */
-size_t 		ft_align_size(size_t size, size_t multiple);
-size_t		ft_header_size(void);
-void		ft_bzero(void *s, size_t n);
-void		*ft_memset(void *b, int c, size_t len);
+size_t					ft_align_size(size_t size, size_t multiple);
+size_t					ft_header_size(void);
+void					ft_bzero(void *s, size_t n);
+void					*ft_memset(void *b, int c, size_t len);
 
 /*
 ** STATS
 */
-int			ft_mode_stat(void);
-void		ft_stat_malloc(int i);
-void		ft_stat_free(void);
-void		ft_stat_defrag(void);
+int						ft_mode_stat(void);
+void					ft_stat_malloc(int i);
+void					ft_stat_free(void);
+void					ft_stat_defrag(void);
 
 /*
 ** LOGS
 */
-int			ft_mode_log(void);
-void		ft_log(int f, t_block *block);
+int						ft_mode_log(void);
+void					ft_log(int f, t_block *block);
 
 /*
 ** DISPLAY
 */
-void		ft_print_memory(const void *addr, size_t size);
-void		ft_putnbr(int n);
-void		ft_putnbr_fd(int fd, int n);
-void		ft_putnbr_str(char *str, int n);
-void		ft_putchar(char c);
-void		ft_putchar_fd(int fd, char c);
-void		ft_putstr(char const *s);
-void		ft_putstr_fd(int fd, char const *s);
-void		ft_putaddr(unsigned long long p);
-void		ft_putaddr_fd(int fd, unsigned long long p);
-void		ft_putaddr_endl(unsigned long long p);
+void					ft_print_memory(const void *addr, size_t size);
+void					ft_putnbr(int n);
+void					ft_putnbr_fd(int fd, int n);
+void					ft_putnbr_str(char *str, int n);
+void					ft_putchar(char c);
+void					ft_putchar_fd(int fd, char c);
+void					ft_putstr(char const *s);
+void					ft_putstr_fd(int fd, char const *s);
+void					ft_putaddr(unsigned long long p);
+void					ft_putaddr_fd(int fd, unsigned long long p);
+void					ft_putaddr_endl(unsigned long long p);
 
 #endif
