58ba842035b0f18ccc8eb20bf934b3590abdfa8d
Sarah Franc <sfranc@MacBook-Air-de-Sarah.local>
2018-07-05 12:07:27 +0200

    logs

--- srcs/logs.c
+++ srcs/logs.c
@@ -30,9 +30,11 @@ static void	ft_print_log_line(int fd, char *s, t_block *block)
 	if (block)
 	{
 		ft_putstr_fd(fd, " ** ");		
-		ft_putaddr_fd(fd, (unsigned long long)((char*)block + ft_header_size()));
+		ft_putaddr_fd(fd, (unsigned long long)((char*)block\
+			+ ft_header_size()));
 		ft_putstr_fd(fd, " - ");
-		ft_putaddr_fd(fd, (unsigned long long)((char*)block + ft_header_size() + block->size));
+		ft_putaddr_fd(fd, (unsigned long long)((char*)block\
+			+ ft_header_size() + block->size));
 		ft_putstr_fd(fd, " : ");
 		ft_putnbr_fd(fd, block->size);
 		ft_putstr_fd(fd, " octets -");
@@ -48,7 +50,8 @@ void	ft_log(int f, t_block *block)
 {
 	int	fd;
 
-	fd = open(LOG_FILE, O_RDWR | O_APPEND | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	fd = open(LOG_FILE, O_RDWR | O_APPEND | O_CREAT,\
+		S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 	if (fd == -1)
 		return ;
 	if (f == REALLOC)
